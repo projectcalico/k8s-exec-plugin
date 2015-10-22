14f432f22dd8be6f9c5c3328e46dfaacdd2fbb2d
Luke Mino-Altherr <luke.mino-altherr@metaswitch.com>
2015-10-22 11:44:13 -0700

    Remove stdout logging in status hook

--- calico_kubernetes/logutils.py
+++ calico_kubernetes/logutils.py
@@ -30,8 +30,6 @@ def configure_logger(logger, log_level, root_logger=False, log_dir=LOG_DIR):
     file_hdlr = logging.handlers.RotatingFileHandler(filename=log_dir+'calico.log',
                                                      maxBytes=10000000,
                                                      backupCount=5)
-    stdout_hdlr = logging.StreamHandler(sys.stdout)
-    stdout_hdlr.setLevel(logging.INFO)
 
     # Determine which formatter to use.
     if root_logger:
@@ -41,8 +39,30 @@ def configure_logger(logger, log_level, root_logger=False, log_dir=LOG_DIR):
 
     # Set formatters on handlers
     file_hdlr.setFormatter(formatter)
-    stdout_hdlr.setFormatter(formatter)
 
     logger.addHandler(file_hdlr)
-    logger.addHandler(stdout_hdlr)
     logger.setLevel(log_level)
+
+def configure_stdout_logger(logger, log_level=logging.INFO, root_logger=False):
+    """
+    Configure an stdout logging handler to the logger
+
+    If a log level is not indicated use the logger's current logging level
+
+    :param logger:  logger object to configure
+    :param log_level: log_level: level at which logger starts logging.
+    :return:
+    """
+    # Determine which formatter to use.
+    if root_logger:
+        formatter = logging.Formatter(ROOT_LOG_FORMAT)
+    else:
+        formatter = logging.Formatter(LOG_FORMAT)
+
+    # Create the handler and apply attributes
+    stdout_hdlr = logging.StreamHandler(sys.stdout)
+    stdout_hdlr.setLevel(log_level)
+    stdout_hdlr.setFormatter(formatter)
+
+    # Add handler to the logger object
+    logger.addHandler(stdout_hdlr)
